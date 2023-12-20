6eb15163fb627348d1fc2ca1df4e03e8e6e965f0
IagoraNz <iago.almeida@ufpi.edu.br>
2023-12-20 13:04:25 -0300

    Backup

--- Ponteiro/a29.c
+++ Ponteiro/a29.c
@@ -16,7 +16,7 @@ int main(){
     printf("Digite o tamanho de colunas: ");
     scanf("%d", &col);
 
-    if((lin * col) % 2 == 1){
+    if(((lin * col) % 2 == 1) && lin == col){
         int **mat = (int **)malloc(lin * sizeof(int *));
         for(i = 0; i < lin; i++){
             mat[i] = (int *)malloc(col * sizeof(int));
@@ -30,7 +30,7 @@ int main(){
         free(mat);
     }
     else{
-        printf("Matriz deve ser impar!");
+        printf("Matriz deve ser impar e quadrada!");
     }
 
     return 0;
@@ -44,10 +44,14 @@ void cruz(int **mat, int lin, int col){
             if(i == lin / 2 || j == col / 2){
                 mat[i][j] = 1;
             }
+            else if(i == j){
+                mat[i][j] = 1;
+            }
             else{
                 mat[i][j] = 0;
             }
         }
+        mat[i][col - 1 - i] = 1;
     }
     exibir(mat, lin, col);
 }
