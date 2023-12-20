35d105db30cedbf25e5fbc90056f87988bb669fd
IagoraNz <iago.almeida@ufpi.edu.br>
2023-12-20 08:30:13 -0300

    Backup

---
+++ Ponteiro/a32.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Dada uma matriz dinâmica, somar os 4 extremos de uma matriz
+
+void preencher(int **mat, int lin, int col);
+void exibir(int **mat, int lin, int col);
+void soma(int **mat, int lin, int col);
+
+int main(){
+
+    int i, lin, col;
+
+    printf("Digite o tamanho de linhas: ");
+    scanf("%d", &lin);
+
+    printf("Digite o tamanho de colunas: ");
+    scanf("%d", &col);
+
+    int **mat = (int **)malloc(lin * sizeof(int *));
+    for(i = 0; i < lin; i++){
+        mat[i] = (int *)malloc(col * sizeof(int));
+    }
+
+    preencher(mat, lin, col);
+    printf("\nMatriz: \n");
+    exibir(mat, lin, col);
+
+    soma(mat, lin, col);
+
+    return 0;
+}
+
+void preencher(int **mat, int lin, int col){
+    int i, j;
+
+    for(i = 0; i < lin; i++){
+        for(j = 0; j < col; j++){
+            printf("Matriz[%d][%d]: ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+}
+
+void exibir(int **mat, int lin, int col){
+    int i, j;
+
+    for(i = 0; i < lin; i++){
+        for(j = 0; j < col; j++){
+            printf("%d\t", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void soma(int **mat, int lin, int col){
+    int i, j, soma = 0;
+
+    for(i = 0; i < lin; i++){
+        for(j = 0; j < col; j++){
+            if((i == 0 && j == 0) || (i == 0 && j == col - 1) || (i == lin - 1 && j == 0) || (i == lin - 1 && j == col - 1)){
+                soma += mat[i][j];
+            }
+        }
+    }
+    printf("\nA soma dos quatro cantos: %d", soma);
+}
